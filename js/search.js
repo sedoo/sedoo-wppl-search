85819a9f2f8262886c461d0b7a46495f13c30c70
ngruwe <nicolas.gruwe@obs-mip.fr>
2020-05-19 11:02:02 +0200

    bugfix for post_per_page

--- js/search.js
+++ js/search.js
@@ -1,34 +1,31 @@
 jQuery(document).ready(function(){   
 
-
+    var cpt;
     jQuery('.sedoo_search_button').click(function() {
         jQuery('#sedoo_search_results_list_js').empty();
         jQuery('.sedoo_search_button.active').removeClass('active');
-        jQuery(this).addClass('active');
-        var array_id = jQuery(this).attr('array_id');   
+        jQuery(this).addClass('active');  
         var trigger_name = jQuery(this).attr('id');  
         var infinite_load_ajout = 0; 
         var previous_state;
-        var cpt = trigger_name.substr(17); 
+        cpt = trigger_name.substr(17); 
             jQuery.ajax({
                 url: ajaxurl,
                 type: "POST",
                 data: {
                   'action': 'search_load_post',
-                  'array_id': array_id,
                   'page_no': 1,
                   'cpt': cpt,
                   'string_search' :search_string
                 }
               }).done(function(response) {
                 jQuery('#sedoo_search_results_list_js').append(response);
-                jQuery('#sedoo_search_results_list_js').append('<div class="sedoo_infiniteloader" id="infiniteloader"> LOADING </div>');
+                jQuery('#sedoo_search_results_list_js').append('<div class="sedoo_infiniteloader" id="infiniteloader"> LOADING... PLEASE WAIT </div>');
 
                 jQuery('#infiniteloader').hide();
                 var count = 2;
                 var total = jQuery('#sedoo_Search_page_count').text();
                 jQuery(window).scroll(function(){
-                    console.log(jQuery('#sedoo_search_results_list_js').offset().top + jQuery('#sedoo_search_results_list_js').outerHeight() - window.innerHeight);
                     if(  (jQuery('#sedoo_search_results_list_js').offset().top + jQuery('#sedoo_search_results_list_js').outerHeight() - window.innerHeight)-20 < jQuery(window).scrollTop() &&  (jQuery('#sedoo_search_results_list_js').offset().top + jQuery('#sedoo_search_results_list_js').outerHeight() - window.innerHeight)+20 > jQuery(window).scrollTop() ) {
                         if (count > total){
                             return false;
@@ -70,7 +67,7 @@ jQuery(document).ready(function(){
                         jQuery('#infiniteloader').remove();
                         jQuery("#sedoo_search_results_list_js").append(html);
                         infinite_load_ajout=0;
-                        jQuery('#sedoo_search_results_list_js').append('<div class="sedoo_infiniteloader" id="infiniteloader"> LOADING </div>'); 
+                        jQuery('#sedoo_search_results_list_js').append('<div class="sedoo_infiniteloader" id="infiniteloader"> LOADING... PLEASE WAIT </div>'); 
                         jQuery('#infiniteloader').hide();
                       }
                     });
--- sedoo-wppl-search.php
+++ sedoo-wppl-search.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sedoo - Search
  * Description: Change l'affichage des resultats de recherche et propose un filtre par cpt
- * Version: 0.0.6
+ * Version: 0.0.7
  * Author: Nicolas Gruwe & Pierre Vert - SEDOO DATA CENTER
  * Author URI:      https://www.sedoo.fr 
  * GitHub Plugin URI: sedoo/sedoo-wppl-search
@@ -46,8 +46,9 @@ function sedoo_search_ajax() {
   $args = array(
     'post_type' => $cpt,
     's' => $search_string,
-    "posts_per_page" => 10,
-    'paged' => $page
+    "posts_per_page" => 20,
+    'paged' => $page,
+    'post_status' => 'publish'
   );
   $searching_post_query = new WP_Query($args);
 
